6bf1c04b4a8ebf6a2ad07750ae8a6c4f08b17f93
Rafael-Fassini <rafaelfassini2006@outlook.com>
2024-04-05 00:08:51 -0300

    FrontEnd do formulário para cadastro de missão e Executar missão (visualização de dados) finalizados. Mas o formulário para cadastro deverá sofrer modificações em breve!

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
-const { ipcMain } = require('electron');
-const { app, BrowserWindow } = require('electron/main');
+const { app, BrowserWindow, screen } = require('electron');
+
+let mainWindow;
 
 const createWindow = () => {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+
+  mainWindow = new BrowserWindow({
+    width: width,
+    height: height,
+    resizable: false // Não permitir redimensionamento
   });
 
-  win.loadFile('views/mainMenu.html');
+  mainWindow.loadFile('views/executeMission.html');
 
+  // Centralizar a janela na tela
+  mainWindow.center();
 };
-
 
 app.whenReady().then(() => {
   createWindow();
@@ -18,15 +23,34 @@ app.whenReady().then(() => {
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
-    };
+    }
+  });
+
+  // Redimensionar a janela quando um novo monitor é adicionado
+  app.on('display-added', () => {
+    if (mainWindow) {
+      const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+      mainWindow.setSize(width, height);
+      mainWindow.center(); // Centralizar a janela após redimensionar
+    }
+  });
+
+  // Redimensionar a janela quando um monitor é removido
+  app.on('display-removed', () => {
+    if (mainWindow) {
+      const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+      mainWindow.setSize(width, height);
+      mainWindow.center(); // Centralizar a janela após redimensionar
+    }
   });
 });
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
-  };
+  }
 });
+
 
 
 
